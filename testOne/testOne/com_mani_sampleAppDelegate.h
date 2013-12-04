11b6fec6e1af17216dd8dd23b832063ebe25e043
mranganathan <mranganathan@paypal.com>
2013-12-03 19:48:46 -0700

    Initial Commit for the testFirstApp

---
+++ testOne/testOne/com_mani_sampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  com_mani_sampleAppDelegate.h
+//  testOne
+//
+//  Created by Ranganathan, Manivannan on 12/3/13.
+//  Copyright (c) 2013 Ranganathan, Manivannan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_mani_sampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testOne/testOne/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testOne
+//
+//  Created by Ranganathan, Manivannan on 12/3/13.
+//  Copyright (c) 2013 Ranganathan, Manivannan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "com_mani_sampleAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([com_mani_sampleAppDelegate class]));
+    }
+}
